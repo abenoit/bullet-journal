95c47a72ea5aab5e3bbd3f5d2c2bbfeef0f1d1a7
Amélie <amelie.benoit33@gmail.com>
2019-01-05 10:11:13 -0500

    :white_check_mark: Configure and add test

---
+++ src/components/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello world !</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  }
+});
---
+++ src/components/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "../App";
+
+describe("App", () => {
+  it("renders correctly with defaults", () => {
+    const button = renderer.create(<App />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+});
